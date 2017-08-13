6ebfa46e96be148cc5819a0788049b356cb461d2
Wilmer Carranza <wcarranzam91@gmail.com>
2017-08-12 18:33:04 -0600

    Muestra solo el contenido de la solicitud en consola

--- src/servidorweb/ServidorWeb.java
+++ src/servidorweb/ServidorWeb.java
@@ -5,7 +5,10 @@
  */
 package servidorweb;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +17,7 @@ import java.net.Socket;
  * @author Wilmer
  */
 public class ServidorWeb extends Thread {
+
     private final int PUERTO = 80;
     private ServerSocket socket;
 
@@ -28,6 +32,15 @@ public class ServidorWeb extends Thread {
                 //No pasa del accept() hasta que entre una solicitud
                 Socket solicitud = socket.accept();
                 System.out.println("Nueva solicitud");
+                try (
+                        //Contiene la respuesta
+                        DataOutputStream out = new DataOutputStream(solicitud.getOutputStream());
+                        //Contiene el contenido de la solicitud
+                        BufferedReader in = new BufferedReader(new InputStreamReader(solicitud.getInputStream()))) {
+                    String input;
+                    while ((input = in.readLine()) != null)
+                        System.out.println(input);
+                }
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
